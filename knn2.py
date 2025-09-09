73fd90d1e0da0a687b7dc37f39a0cd6a8d6f9441
Sarojith-ks <sarojithks@gmail.com>
2025-09-09 14:40:50 +0530

    Add files via upload

---
+++ knn2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+df = pd.read_csv('tshirt.csv')
+X = df[['Height', 'Weight']]
+y = df['Tshirt_size']
+row1=df.iloc[1:]
+print(row1)
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X, y)
+a = int(input("Enter height: "))
+b = int(input("Enter weight: "))
+sample = [[a, b]]
+predicted_size = knn.predict(sample)
+print(f"The predicted t-shirt size is: {predicted_size[0]}")
